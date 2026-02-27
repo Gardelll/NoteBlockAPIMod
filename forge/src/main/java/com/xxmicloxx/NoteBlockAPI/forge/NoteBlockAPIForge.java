58c7a553adaecaf4e5600fb2688be5268949278c
Gardel <gardel741@outlook.com>
2026-02-27 10:21:53 +0800

    fix: forge mod load

--- forge/src/main/java/com/xxmicloxx/NoteBlockAPI/forge/NoteBlockAPIForge.java
+++ forge/src/main/java/com/xxmicloxx/NoteBlockAPI/forge/NoteBlockAPIForge.java
@@ -2,14 +2,14 @@ package com.xxmicloxx.NoteBlockAPI.forge;
 
 import com.xxmicloxx.NoteBlockAPI.NoteBlockAPI;
 import dev.architectury.platform.forge.EventBuses;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(NoteBlockAPI.MOD_ID)
 public final class NoteBlockAPIForge {
-    public NoteBlockAPIForge(IEventBus modBus) {
+    public NoteBlockAPIForge(FMLJavaModLoadingContext context) {
         // Submit our event bus to let Architectury API register our content on the right time.
-        EventBuses.registerModEventBus(NoteBlockAPI.MOD_ID, modBus);
+        EventBuses.registerModEventBus(NoteBlockAPI.MOD_ID, context.getModEventBus());
 
         // Run our common setup.
         NoteBlockAPI.init();
